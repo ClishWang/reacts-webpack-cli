368944f788c86701a0e8611ef449caf2e279dd4c
ClishWang <wangmingzhe@qiyi.com>
2021-06-01 21:44:11 +0800

    fix: babel options

--- config/BabelConfig.js
+++ config/BabelConfig.js
@@ -4,6 +4,7 @@ module.exports = function () {
         "presets": ["@babel/preset-env", "@babel/preset-react"],
         "plugins": [
             ["@babel/plugin-proposal-decorators", {"legacy": true}],
+            ["@babel/plugin-proposal-private-methods", { "loose": true }],
             ["@babel/plugin-proposal-class-properties", { "loose": true }],
             "@babel/plugin-proposal-export-default-from",
             "@babel/plugin-proposal-export-namespace-from",
