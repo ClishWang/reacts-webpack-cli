559c0c4ad63457f7dca04633d464c05827df6869
clish <clish@clishdeMacBook-Pro.local>
2021-11-19 11:04:58 +0800

    feat: add ?. ?? syntax

--- config/BabelConfig.js
+++ config/BabelConfig.js
@@ -7,6 +7,8 @@ module.exports = function () {
             ["@babel/plugin-proposal-private-methods", { "loose": true }],
             ["@babel/plugin-proposal-class-properties", { "loose": true }],
             ["@babel/plugin-proposal-private-property-in-object", { "loose": true }],
+            "@babel/plugin-proposal-nullish-coalescing-operator",
+            "@babel/plugin-proposal-optional-chaining",
             "@babel/plugin-proposal-export-default-from",
             "@babel/plugin-proposal-export-namespace-from",
             "@babel/plugin-transform-runtime"
